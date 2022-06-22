6d4441d9fab7e2e9f122dd488e4a4332061e8c02
Nicolas BENAIS <nicolasbenais@MacBook-Air-de-Nicolas.local>
2022-06-22 17:15:06 +0200

    pictures.js done

--- routes/pictures.js
+++ routes/pictures.js
@@ -7,8 +7,12 @@ const Picture = require("../models/Picture");
 router.get("/pictures", async (req, res) => {
   try {
     let limit = 12;
+    if (req.query.limit) {
+      limit = limit + req.query.limit;
+    }
 
-    const pictures = await Picture.find().limit(limit);
+    const pictures = await Picture.find().sort({ fields: -1 }).limit(limit);
+
     res.status(200).json(pictures);
   } catch (error) {
     console.log(error.message);
